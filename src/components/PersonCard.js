43688e11c98726cc9b7f6cdafabb450290f62aca
Karito2022 <reyesbarrerakaren@gmail.com>
2022-04-09 20:35:27 -0500

    Finalizado

---
+++ src/components/PersonCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class PersonCard extends Component {
+    render(){
+        const { firstName, lastName, age, hairColor }= this.props;
+        return <div>
+            <h2>{ lastName }, { firstName }</h2>
+            <p>Age: {this.props.age}</p>
+            <p>Hair Color: { hairColor }</p>
+        </div>
+    }
+}
+
+export default PersonCard;
